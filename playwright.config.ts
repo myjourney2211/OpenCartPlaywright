d25fd2bb9f46ee395d303c1af79d754b58ef20b6
Vijay <ajwheuuwhhdhhs@outlook.com>
2026-07-24 18:41:12 +0530

    updated playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
 
   testDir: "./tests",
   fullyParallel: true,
@@ -23,6 +23,8 @@ export default defineConfig({
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true, // Ignore SSL errors if any
     permissions: ['geolocation'], //Set necessary permission for geolocation based tests
+    actionTimeout: 15000,
+    navigationTimeout: 30000,
   },
 
   //grep: /@master/,
@@ -33,14 +35,14 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
 
-/*     {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    }, */
+    /*     {
+          name: "firefox",
+          use: { ...devices["Desktop Firefox"] },
+        },
+    
+        {
+          name: "webkit",
+          use: { ...devices["Desktop Safari"] },
+        }, */
   ],
 });
